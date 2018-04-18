c4e7003cef83f517f5783bda751d379a9cd1eece
Vladimir Rusanov <vrus9787@gmail.com>
2018-04-18 07:38:21 +0300

    started to apply theme

--- src/PTK/Templates/App.fs
+++ src/PTK/Templates/App.fs
@@ -3,37 +3,61 @@ module App
 open Giraffe.GiraffeViewEngine
 
 let layout (content: XmlNode list) =
-    html [_class "has-navbar-fixed-top"] [
+    html [_class "no-js"; _lang "en"] [
         head [] [
             meta [_charset "utf-8"]
-            meta [_name "viewport"; _content "width=device-width, initial-scale=1" ]
-            title [] [encodedText "Hello PTK"]
-            link [_rel "stylesheet"; _href "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" ]
-            link [_rel "stylesheet"; _href "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css" ]
-            link [_rel "stylesheet"; _href "/app.css" ]
+            meta [_name "viewport"; _content "width=device-width, initial-scale=1, maximum-scale=1" ]
+            title [] [encodedText "PTK. Keeping my thoughts organized"]
+            meta [_name "description"; _content "Personal tool kit"]
+            meta [_name "author"; _content "3615"]
 
+            link [_rel "stylesheet"; _href "css/default.css" ]
+            link [_rel "stylesheet"; _href "css/layout.css" ]
+            link [_rel "stylesheet"; _href "css/media-queries.css" ]
+
+            script [_src "js/modernizr.js"] []
+
+            link [_rel "shortcut icon"; _href "favicon.png"]
         ]
         body [] [
-            yield nav [ _class "navbar is-fixed-top has-shadow" ] [
-                div [_class "navbar-brand"] [
-                    a [_class "navbar-item"; _href "/"] [
-                        img [_src "https://avatars0.githubusercontent.com/u/35305523?s=200"; _width "28"; _height "28"]
-                    ]
-                    div [_class "navbar-burger burger"; attr "data-target" "navMenu"] [
-                        span [] []
-                        span [] []
-                        span [] []
+            yield header [ _id "top" ] [
+                div [_class "row"] [
+                    div [_class "header-content twelve columns"] [
+                        h1 [_id "logo-text"] [
+                            a [_href "/"; _title "ad"] [
+                                rawText "Keep It Simple."
+                            ]
+                        ]
+                        p [_id "intro"] [
+                            rawText "Put your awesome slogan here..."
+                        ]
                     ]
                 ]
-                div [_class "navbar-menu"; _id "navMenu"] [
-                    div [_class "navbar-start"] [
-                        a [_class "navbar-item"; _href "/mems"] [rawText "Memories3615"]
-                        a [_class "navbar-item"; _href "/tools"] [rawText "Tool kit"]
-                        a [_class "navbar-item"; _href "/contacts"] [rawText "Contacts"]
+                nav [_id "nav-wrap"] [
+                    a [_class "mobile-btn"; _href "#nav-wrap"; _title "Show navigation"][rawText "Show Menu" ]
+                    a [_class "mobile-btn"; _href "#n"; _title "Hide navigation"][rawText "Hide Menu" ]
+                    div [_class "row"] [
+                        ul [_id "nav"; _class "nav"][
+                            li [_class "current"] [
+                                a [_href "/"] [rawText "Home"]
+                            ]
+                            li [] [
+                                a [_href "mems"] [rawText "Memories3615"]
+                            ]
+                            li [] [
+                                a [_href "/tools"] [rawText "Tool kit"]
+                            ]
+                            li [] [
+                                a [_href "/contacts"] [rawText "Contacts"]
+                            ]
+                        ]
                     ]
                 ]
             ]
-            yield! content
+            yield div [_id "content-wrap"][
+                yield! content
+            ]
+            
             yield footer [_class "footer is-fixed-bottom"] [
                 div [_class "container"] [
                     div [_class "content has-text-centered"] [
@@ -46,6 +70,10 @@ let layout (content: XmlNode list) =
                     ]
                 ]
             ]
-            yield script [_src "/app.js"] []
+
+            yield script [_type "text/javascript"; _src "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"] []            
+            yield script [] [rawText """window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')"""]
+            yield script [_type "text/javascript"; _src "js/jquery-migrate-1.2.1.min.js"] []
+            yield script [_type "text/javascript"; _src "js/main.js"] []
         ]
     ]
