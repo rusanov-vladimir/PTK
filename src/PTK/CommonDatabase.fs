5caa0885b095b2d74cd592e6f51bb0ef82f9e92b
Vladimir Rusanov <vrus9787@gmail.com>
2019-02-25 21:17:20 +0200

    Fixed db query to return correct mem count

--- src/PTK/CommonDatabase.fs
+++ src/PTK/CommonDatabase.fs
@@ -6,8 +6,12 @@ open System.Threading.Tasks
 open Common.Models
 
 module Database =
-
-  let getAll connectionString : Task<Result<seq<CategoryWithCount>, exn>> =
+  let getAll (connectionString:string) : Task<Result<CategoryWithCount seq, exn>> =
     queueConnection connectionString (fun connection -> 
-      query connection "SELECT id, title, description FROM Categories" None)
-    
+      query connection "
+        SELECT c.id, c.title, count(m.id) as postCount FROM Categories c
+        LEFT JOIN Mems m on c.id = m.categoryId
+        GROUP BY c.id, c.title
+        ORDER BY count(m.id) desc
+        FETCH FIRST 5 rows only
+      " None)
